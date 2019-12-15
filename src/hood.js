ae10aa89064376221f88ddce919147befac652ba
Brian Zelip <bzelip@gmail.com>
2019-12-15 14:31:29 -0500

    hood.js: Log NOLA hoods via IIFE

--- src/hood.js
+++ src/hood.js
@@ -1,1 +1,15 @@
-console.log('hello from hood.js!');
+(function() {
+  const xmlhttp = new XMLHttpRequest(),
+    method = 'GET',
+    url = 'https://data.nola.gov/resource/abhb-x4ch.geojson';
+
+  xmlhttp.open(method, url, true);
+
+  xmlhttp.onload = function() {
+    console.log(
+      JSON.parse(this.responseText).features.map(f => f.properties.gnocdc_lab)
+    );
+  };
+
+  xmlhttp.send();
+})();
